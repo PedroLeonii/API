6b1197855ef2221dfe8c8571b1b83f59cf49c115
Pedro Leoni <pedro.leoni@marconirovereto.it>
2020-02-29 14:20:22 +0100

    previsioni html

--- js/dati.js
+++ js/dati.js
@@ -1,13 +1,42 @@
-var options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-}
-navigator.geolocation.getCurrentPosition(success,error,options)
+window.onload = ()=>{    
+   var forecastWeek = document.getElementById("forecastWeek");
 
-function success(position){
-    
+    function getForecastGeoloc(){    
+        return new Promise((resolve,reject)=>{navigator.geolocation.getCurrentPosition(resolve,reject)})
+    }
+
+    function requestForecas(lon,lat){
+        return fetch("https://api.weatherbit.io/v2.0/forecast/daily?&lat="+lat+"&lon="+lon+"&key=36d53d5360cf4756940daa069b8acf46")
+    }
+
+    function Init(){
+        getForecastGeoloc().then((position)=>{
+            requestForecas(position.coords.longitude,position.coords.latitude).then(response => {
+                if(response.ok){
+                    response.json().then((date)=>{
+                        console.log(date)
+                        setForecastOut(date,outputDaily,outputWeek);
+                    })
+                }
+                else{
+                    
+                }
+            })
+        })
+    }
+    function setForecastOut(date,...parentNodes){
+
+    }
+    Init()
 }
-function error(er){
-    console.log("ERROR CODE: "+er.code+"    ERROR TYPE: "+er.message);
-}
+
+/* requestForecas(position.coords.longitude,position.coords.latitude).then(response => {
+    if(response.ok){
+        response.json().then(dati=>{
+            resolve(dati)
+        })
+    }
+    else{
+        reject(response)
+    }
+}) */
