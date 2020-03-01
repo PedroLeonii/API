001b33e3fa4fe5d2158a8297d68f6772736ac001
Pedro Leoni <pedro.leoni@marconirovereto.it>
2020-03-01 15:39:46 +0100

    previsioni geolocalizzazione

--- js/dati.js
+++ js/dati.js
@@ -1,5 +1,10 @@
 window.onload = ()=>{    
-   var forecastWeek = document.getElementById("forecastWeek");
+    var daySlider = 0;
+    const ids = ["valid_date","temp","max_temp","min_temp","wind_cdir_full","wind_spd"];
+    var loading = document.getElementById("loading");
+    var forecastWeek = document.getElementById("forecastWeek");
+    var btPrev = document.getElementById("previous");
+    var btNext = document.getElementById("next");
 
     function getForecastGeoloc(){    
         return new Promise((resolve,reject)=>{navigator.geolocation.getCurrentPosition(resolve,reject)})
@@ -9,25 +14,59 @@ window.onload = ()=>{
         return fetch("https://api.weatherbit.io/v2.0/forecast/daily?&lat="+lat+"&lon="+lon+"&key=36d53d5360cf4756940daa069b8acf46")
     }
 
-    function Init(){
-        getForecastGeoloc().then((position)=>{
-            requestForecas(position.coords.longitude,position.coords.latitude).then(response => {
-                if(response.ok){
-                    response.json().then((date)=>{
-                        console.log(date)
-                        setForecastOut(date,outputDaily,outputWeek);
-                    })
-                }
-                else{
-                    
-                }
-            })
-        })
+    function outForecast(){
+        if (daySlider < 7 && daySlider >= 0) {
+            getForecastGeoloc().then((position)=>{
+                requestForecas(position.coords.longitude,position.coords.latitude).then(response => {
+                    if(response.ok){
+                        response.json().then((data)=>{
+                        
+                            setForecastOut(data);
+                        })
+                    }
+                    else{
+                        
+                    }
+                })
+            })   
+        }
     }
-    function setForecastOut(date,...parentNodes){
+    function setForecastOut(info){
+        var location = document.getElementById("location");
+        var description = document.getElementById("description");
+        var imageW = document.getElementById("whetherImg");
 
+        description.innerHTML = info.data[daySlider].weather.description;
+        imageW.setAttribute("src","https://www.weatherbit.io/static/img/icons/"+info.data[daySlider].weather.icon+".png")
+        location.innerHTML = info.city_name +" "+ info.country_code;
+        
+        ids.forEach(id => {
+            var output = document.getElementById(id);
+            var testo = info.data[daySlider][id];
+            output.innerHTML = testo;
+        });
+        appearAnim(forecastWeek);
     }
-    Init()
+    function init(){
+        outForecast();
+        setTimeout(()=>{
+            loading.style.display = "none";
+            forecastWeek.style.removeProperty("display");    
+        },1000)
+    }
+    outForecast();
+    btNext.addEventListener("click",()=>{
+        daySlider++;
+        outForecast();
+    })
+    btPrev.addEventListener("click",()=>{
+        daySlider--;
+        outForecast();
+    })
+    function appearAnim(node){
+        node.animate([{opacity:0,marginLeft: "6%",marginRight:"6%"},{opacity:1,marginLeft: "0%",marginRight:"0%"}],600)
+    }
+    init();
 }
 
 /* requestForecas(position.coords.longitude,position.coords.latitude).then(response => {
